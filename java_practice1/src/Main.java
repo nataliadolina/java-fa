ce2eb72d81eb5bfba34ceb2df3749b23039a73a5
Natalia <nat.dol30@mail.ru>
2022-09-29 18:35:02 +0300

    Added get power of Matrix function.ended up with matrix

--- java_practice1/src/Main.java
+++ java_practice1/src/Main.java
@@ -1,5 +1,3 @@
-import java.nio.charset.StandardCharsets;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -12,9 +10,27 @@ public class Main {
         Matrix matrix4 = new Matrix(new Float[]{5f, 3f, 7f, 0f, 7f, 1f, 9f, 2f, 3f, 4f, 7f, 6f}, 3, 4, null);
         Matrix matrix5 = new Matrix(new Float[]{20.30f, 1.60f, 18.60f, 1.30f, 12.10f, 1.00f, 23.00f, 1.80f}, 4, 2, null);
         printOperation(matrix4, matrix5.VisualString(), matrix4.mulMatrix(matrix5), "*");
+
+        Matrix matrixToTranspose = new Matrix(new Float[]{7f, 9f,10f, 7.4f, 1.2f, 8.4f, 12.5f}, 3, 2, null);
+        Matrix transposedMatrix = matrixToTranspose.transpose();
+        printTransposeOperation(matrixToTranspose, transposedMatrix);
+
+        Matrix matrixToPow = new Matrix(new Float[]{3f, 5f, 2f, 1f}, 2, 2, null);
+        Matrix poweredMatrix3 = matrixToPow.powMatrix(3);
+        Matrix poweredMatrix0 = matrixToPow.powMatrix(0);
+        printPowOperation(matrixToPow, poweredMatrix3, 3);
+        printPowOperation(matrixToPow, poweredMatrix0, 0);
     }
 
     private static void printOperation(Matrix arg1, String arg2, Matrix result, String operation){
         System.out.println(arg1.VisualString() + operation + arg2 + " = " + result.VisualString());
+    }
+
+    private static void printTransposeOperation(Matrix matrix, Matrix transposedMatrix){
+        System.out.println(String.format("Transposed %s = %s", matrix.VisualString(), transposedMatrix.VisualString()));
+    }
+
+    private static void printPowOperation(Matrix matrix, Matrix poweredMatrix, int power){
+        System.out.println(String.format("%s raised to power of " + String.valueOf(power) + " = %s", matrix.VisualString(), poweredMatrix.VisualString()));
     }
 }
--- java_practice1/src/Matrix.java
+++ java_practice1/src/Matrix.java
@@ -2,7 +2,7 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 
-public class Matrix {
+public class Matrix{
     private final float VALUE_FOR_EMPTY_CELLS;
     private final float DEFAULT_VALUE = 1;
     private int _numRows;
@@ -18,6 +18,27 @@ public class Matrix {
         _numCols = numCols != null && numCols > 0 ? numCols: default_size;
         VALUE_FOR_EMPTY_CELLS = defaultValue != null ? defaultValue : DEFAULT_VALUE;
         createData(data);
+    }
+
+    public Matrix(ArrayList<MatrixElement> data, HashMap<String, Integer> dataMap, Integer numRows, Integer numCols, Float defaultValue){
+        int default_size = 1;
+        _numRows = numRows != null && numRows > 0 ? numRows: default_size;
+        _numCols = numCols != null && numCols > 0 ? numCols: default_size;
+        VALUE_FOR_EMPTY_CELLS = defaultValue != null ? defaultValue : DEFAULT_VALUE;
+        _data = new ArrayList<>();
+        _data.addAll(data);
+        _dataMap = new HashMap<>();
+        _dataMap.putAll(dataMap);
+    }
+
+    public void setNewData(ArrayList<MatrixElement> data) {
+        _data = new ArrayList<>();
+        _data.addAll(data);
+        for (MatrixElement el : _data){
+            int row = el.Row;
+            int col = el.Col;
+            _dataMap.put(getCode(row, col), _data.indexOf(el));
+        }
     }
 
     private void createData(Number[] data){
@@ -58,7 +79,7 @@ public class Matrix {
             int col = el.Col;
             Float newValue = other.get(row, col).Value + el.Value;
             MatrixElement newElement = new MatrixElement(row, col, newValue);
-            newMatrix.set(newElement);
+            newMatrix.set(row, col, newElement);
         }
         return newMatrix;
     }
@@ -82,7 +103,7 @@ public class Matrix {
             int col = el.Col;
             Float newValue = other.get(row, col).Value - el.Value;
             MatrixElement newElement = new MatrixElement(row, col, newValue);
-            newMatrix.set(newElement);
+            newMatrix.set(row, col, newElement);
         }
         return newMatrix;
     }
@@ -95,7 +116,9 @@ public class Matrix {
     public Matrix mulNumber(float number){
         Matrix newMatrix = new Matrix(new Float[]{0f}, _numRows, _numCols, null);
         for (MatrixElement el : _data){
-            newMatrix.set(new MatrixElement(el.Row, el.Col, el.Value * number));
+            int row = el.Row;
+            int col = el.Col;
+            newMatrix.set(row, col, new MatrixElement(row, col, el.Value * number));
         }
         return newMatrix;
     }
@@ -108,6 +131,20 @@ public class Matrix {
      * @throws    IllegalArgumentException  если в матрице {@code this} число {@code _numCols}, не равно {@code other.numRows()}
      */
     public Matrix mulMatrix(Matrix other){
+        int otherCols = other.numCols();
+        Matrix resultMatrix = new Matrix(new Float[]{0f}, _numRows, otherCols, null);
+        mulMatrixInternal(other, resultMatrix, otherCols);
+        return resultMatrix;
+    }
+
+    private Matrix mulMatrixSeveralTimes(Matrix other, Matrix resultMatrix, int size, int n){
+        for (int i = 0; i < n; i++){
+            mulMatrixInternal(other, resultMatrix, size);
+        }
+        return resultMatrix;
+    }
+
+    private void mulMatrixInternal(Matrix other, Matrix newMatrix, int otherCols){
         int otherRows = other.numRows();
         if (_numCols != otherRows){
             throw new IllegalArgumentException("Переданная матрица не удовлетворяет условию \n" +
@@ -115,8 +152,7 @@ public class Matrix {
                     "{@code other} {@code _numRows}");
         }
 
-        int otherCols = other.numCols();
-        Matrix newMatrix = new Matrix(new Float[]{0f}, _numRows, otherCols, null);
+        ArrayList<MatrixElement> _newData = new ArrayList<>();
         for (int i = 0; i < _numRows; i++){
             for (int j=0; j < otherCols; j++){
                 float value = 0;
@@ -124,9 +160,48 @@ public class Matrix {
                     value += get(i, otherI).Value * other.get(otherI, j).Value;
                 }
                 MatrixElement newElement = new MatrixElement(i, j, value);
-                newMatrix.set(newElement);
+                _newData.add(newElement);
             }
         }
+        newMatrix.setNewData(_newData);
+    }
+
+    public Matrix transpose(){
+        Matrix newMatrix = new Matrix(new Float[]{0f}, _numCols, _numRows, null);
+        for (int i = 0; i < _numCols; i++){
+            for (int j = 0; j < _numRows; j++){
+                newMatrix.set(i, j, get(j, i));
+            }
+        }
+        return newMatrix;
+    }
+
+    /**
+     * Возводит матрицу в степень k
+     *
+     * <p>работае только для квадратных матриц.
+     * @return the new Matrix
+     * @throws    IllegalArgumentException  если матрица не квадратная или k < 0}
+     */
+    public Matrix powMatrix(int k){
+        if (_numRows != _numCols){
+            throw new IllegalArgumentException("Невозможно возвести неквадратную матрицу в нулевую степень");
+        }
+
+        if (k < 0){
+            throw new IllegalArgumentException("Матрица может быть возведена только в целое положительное число или 0");
+        }
+
+        if (k == 0){
+            Matrix newMatrix = new Matrix(new Float[]{}, _numRows, _numCols, 0f);
+            for (int i = 0; i< _numRows; i++){
+                newMatrix.set(i, i, new MatrixElement(i, i, 1f));
+            }
+            return newMatrix;
+        }
+
+        Matrix newMatrix = new Matrix(_data, _dataMap, _numRows, _numRows, VALUE_FOR_EMPTY_CELLS);
+        newMatrix.mulMatrixSeveralTimes(this, newMatrix, _numRows, k-1);
         return newMatrix;
     }
 
@@ -144,13 +219,12 @@ public class Matrix {
         _matrixVisualization = listFormatter.generateTable(_numRows + 1, _numCols + 1, header, this);
         return _matrixVisualization;
     }
+
     public MatrixElement get(int row, int col){
         return _data.get(_dataMap.get(getCode(row, col)));
     }
 
-    public void set(MatrixElement element){
-        int row = element.Row;
-        int col = element.Col;
+    public void set(int row, int col, MatrixElement element){
         _data.set(_dataMap.get(getCode(row, col)), element);
     }
 
